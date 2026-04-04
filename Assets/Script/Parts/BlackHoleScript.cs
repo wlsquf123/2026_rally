d969d7b4136444030fc824f2ad66bd7e37ab613c
mind <tlsehdwns7597@gmail.com>
2026-04-04 17:46:22 +0900

    20260404

--- Assets/Script/Parts/BlackHoleScript.cs
+++ Assets/Script/Parts/BlackHoleScript.cs
@@ -7,15 +7,7 @@ public class BlackHoleScript : MonoBehaviour
     Transform Player;
     void Start()
     {
-        Player = GameObject.FindWithTag("Player").transform;
-        // 방금 막 게임매니저에서 소환됨.
-        // 1. 플레이어 위치로 텔레포트
-        // 2. 블랙홀을 N초 뒤에 삭제
-
-        transform.position = Player.transform.position;
-        // N초 뒤에 삭제(변수 파세요~)
-
-        StartCoroutine(BlackHoleDelay(3f));
+        Destroy(gameObject, 3f);
     }
 
 
@@ -25,12 +17,5 @@ public class BlackHoleScript : MonoBehaviour
         {
             Destroy(other.gameObject);
         }
-    }
-
-    public IEnumerator BlackHoleDelay (float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        Destroy(gameObject);
-    }
-    
+    }    
 }
